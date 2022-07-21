8d86cb2f25dd61e0c7753d8b209a25fdca3b807b
Anurag <sanurag4518@gmail.com>
2022-07-21 22:52:29 +0530

    contacts

--- Components/About.js
+++ Components/About.js
@@ -6,7 +6,7 @@ function About() {
     <div name="About" className="h-screen w-full bg-gradient-to-b from-indigo-900 to-indigo-700">
       <div className="max-w-screen-lg mx-auto flex flex-col justify-center items-center h-full px-4 md:flex-row">
         <div>
-          <h2 className="text-3xl sm:text-5xl font-bold text-white">
+          <h2 className="text-3xl sm:text-5xl pb-4 font-bold text-white">
             My name is Anurag and I am a Web Developer
           </h2>
           
--- Components/Contact.js
+++ Components/Contact.js
@@ -33,7 +33,7 @@ function Contact() {
               className="p-2 bg-transparent border text-white focus:outline-none"
             ></textarea>
 
-            <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
+            <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 md:my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
               Submit
             </button>
           </form>
